91f93995845ceff21b9cb61b238cb9e208239c8b
kharioki <tony.kharioki@gmail.com>
2022-12-08 13:51:10 +0300

    assets and config

---
+++ config/data.js
@@ -0,0 +1,63 @@
+export const newsData = [
+  {
+    id: 1,
+    title: "All You Need To know About Night Photography on Smartphones",
+    image: require("./../assets/images/iphone.jpg"),
+    date: "2 Days Ago",
+    author: "James Bowen",
+    avatar: require("./../assets/images/bmw.jpg"),
+    content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa rem ad, praesentium fuga exercitationem eaque earum eius officiis consequuntur voluptatibus vero ex rerum, totam est deserunt neque consectetur recusandae omnis!
+
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa rem ad, praesentium fuga exercitationem eaque earum eius officiis consequuntur voluptatibus vero ex rerum, totam est deserunt neque consectetur recusandae omnis!`,
+  },
+  {
+    id: 2,
+    title: "10 Beautiful Landscapes That Will Make You Appreciate Nature",
+    image: require("./../assets/images/ocean.jpg"),
+    date: "4 Days Ago",
+    author: "Alfred Andoh",
+    avatar: require("./../assets/images/mountain.jpg"),
+    content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa rem ad, praesentium fuga exercitationem eaque earum eius officiis consequuntur voluptatibus vero ex rerum, totam est deserunt neque consectetur recusandae omnis!
+
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa rem ad, praesentium fuga exercitationem eaque earum eius officiis consequuntur voluptatibus vero ex rerum, totam est deserunt neque consectetur recusandae omnis!`,
+  },
+  {
+    id: 3,
+    title: "Drone Technology: The Good & The Bad",
+    image: require("./../assets/images/drone.jpg"),
+    date: "4 Days Ago",
+    author: "James Bowen",
+    avatar: require("./../assets/images/bmw.jpg"),
+    content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa rem ad, praesentium fuga exercitationem eaque earum eius officiis consequuntur voluptatibus vero ex rerum, totam est deserunt neque consectetur recusandae omnis!
+
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa rem ad, praesentium fuga exercitationem eaque earum eius officiis consequuntur voluptatibus vero ex rerum, totam est deserunt neque consectetur recusandae omnis!`,
+  },
+];
+
+export const exploreData = [
+  {
+    id: 1,
+    title: "Tech",
+    image: require("./../assets/images/drone.jpg"),
+  },
+  {
+    id: 2,
+    title: "Sports",
+    image: require("./../assets/images/football.jpg"),
+  },
+  {
+    id: 3,
+    title: "Food",
+    image: require("./../assets/images/cake.jpg"),
+  },
+  {
+    id: 4,
+    title: "Science",
+    image: require("./../assets/images/iphone.jpg"),
+  },
+  {
+    id: 5,
+    title: "Music",
+    image: require("./../assets/images/piano.jpg"),
+  },
+];
---
+++ config/theme.js
@@ -0,0 +1,7 @@
+export const colors = {
+  primary: "#f3f4f6",
+  secondary: "#ffffff",
+  tertiary: "#4b5563",
+  accent: "#0891b2",
+  tint: "#111827",
+};
